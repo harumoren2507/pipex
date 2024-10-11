1f9e791467b6288f1214415b333dd9565de9133e
Ren Toriyama <retoriya@c4r3s7.42tokyo.jp>
2024-10-11 22:30:43 +0900

    pipex infile cat とかはできるようになった

--- test用/parent_process/srcs/parent.c
+++ test用/parent_process/srcs/parent.c
@@ -1,13 +1,11 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdlib.h>
-#include "libft/libft.h"
-
-#define BUFFER_SIZE 1024
+#include "../pipex.h"
 
 void error_message(const char *msg)
 {
-	perror(err_msg);
+	perror(msg);
 	exit(EXIT_FAILURE);
 }
 
@@ -18,7 +16,7 @@ int	main(int argc, char *argv[], char *envp[])
 		error_message(ERR_ARGC);
 	}
 	int input_fd = open(argv[1], O_RDONLY);
-	if (input_fd = -1)
+	if (input_fd == -1)
 		error_message(ERR_PIPE);
 	if (dup2(input_fd, STDIN_FILENO) == -1) {
 		error_message(ERR_DUP);
@@ -28,9 +26,9 @@ int	main(int argc, char *argv[], char *envp[])
 	char *cmd_path = "/bin/";
 	char full_path[BUFFER_SIZE];
 	ft_strlcpy(full_path, cmd_path, BUFFER_SIZE);
-	ft_strlcat(full_path, argv[2], BUFEER_SIZE);
+	ft_strlcat(full_path, argv[2], BUFFER_SIZE);
 
-	char *cmd_args[] = {argc[2], NULL};
+	char *cmd_args[] = {argv[2], NULL};
 	execve(full_path, cmd_args, envp);
 
 	error_message(ERR_EXECVE);
